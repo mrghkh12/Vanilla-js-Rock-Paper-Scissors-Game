28429e3e651a5dc73837575681e17c19cdad4206
mohammad reza <mrghkh12@gmail.com>
2024-08-02 17:04:10 +0330

    feat(app.js): select element

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+const $ = document
+
+const gameContainer = $.querySelector('.container'),
+    userChoice = $.querySelector('.user-choice img'),
+    botChoice = $.querySelector('.bot-choice img'),
+    resultText = $.querySelector('.result'),
+    optionImage = $.querySelectorAll('.option-image .image');
+
